66d578d20a6a13580895836ae58e1974a8fb9a6c
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-17 21:15:27 +0530

    Intro to DOM

---
+++ 11_DOM/script.js
@@ -0,0 +1,7 @@
+// alert("ALERT")
+
+let promptName = prompt("Enter your name: ", "Example") // "Example" is written here as a default value in the prompt
+console.log(promptName)
+document.write("Hello "+promptName)
+
+console.log(document.title)
