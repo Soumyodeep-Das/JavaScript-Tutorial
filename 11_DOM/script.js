42e365ce149a13175d8742b349f5e648ef5026b4
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-17 23:26:25 +0530

    addEventListener()

--- 11_DOM/script.js
+++ 11_DOM/script.js
@@ -9,30 +9,30 @@
 
 //! tag name
 
-const body = document.querySelector("body") // selects the first element matched
+const body = document.querySelector("body"); // selects the first element matched
 // console.log(body) // returns HTMLBodyElement
 // body.innerHTML = "<h1>Hacked</h1>"
 // console.log(body.innerHTML) // returns String
-const code = document.querySelector("code")
+const code = document.querySelector("code");
 // code.innerHTML = "alert('Hello World')"
 
-//! id 
+//! id
 
-const username = document.querySelector("#username")
+const username = document.querySelector("#username");
 // username.innerText = "SDas"
 
 //! class
 
-const block = document.querySelector(".block")
+const block = document.querySelector(".block");
 // console.log(block)
 
-const div = document.querySelector("div")
+const div = document.querySelector("div");
 // console.log(div.parentElement.children)
 
-const p = document.querySelectorAll("p")
+const p = document.querySelectorAll("p");
 p.forEach((ele) => {
-    // console.log(ele.innerHTML)
-})
+  // console.log(ele.innerHTML)
+});
 
 // const ele = document.getElementById("username")
 // console.log(ele.innerText = "SDas")
@@ -47,12 +47,45 @@ p.forEach((ele) => {
 
 // ele.remove() // remove the whole element
 
-const ele = document.getElementsByClassName("block")
+const ele = document.getElementsByClassName("block");
 
 // console.log(ele)
 
-for(let i=0; i<ele.length; i++){
-    if (ele.item(i).tagName.toLowerCase() == 'div'){
-        ele.item(i).style.background = "lightblue"
-    }
+// for(let i=0; i<ele.length; i++){
+//     if (ele.item(i).tagName.toLowerCase() == 'div'){
+//         ele.item(i).style.background = "lightblue"
+//     }
+// }
+
+function handleButtonClick() {
+  alert("Btn clicked");
+}
+
+const btn = document.getElementById("clickButton");
+// btn.onclick = function () {
+//     alert("Btn clicked from function")
+// }
+
+// btn.onclick = () => {
+//     alert("Btn clicked from arrow function")
+// }
+
+// btn.addEventListener('click', handleButtonClick())
+
+// btn.addEventListener('click', () => {
+//     alert("Btn clicked from arrow function")
+// })
+
+const userName = document.getElementById("username");
+
+btn.addEventListener("click", () => {
+  if (userName.style.color === "red") userName.style.color = "blue";
+  else userName.style.color = "red";
+});
+
+const allChildren = document.querySelector("body").children;
+for (let i = 0; i < allChildren.length; i++) {
+  allChildren.item(i).addEventListener("click", () => {
+    allChildren.item(i).remove();
+  });
 }
