bf105b2ddbbe3e062362e392649246ee39c66f9c
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-17 21:33:06 +0530

    document.querySelector();

--- 11_DOM/script.js
+++ 11_DOM/script.js
@@ -1,7 +1,35 @@
-// alert("ALERT")
+// query.selector
+/*
+ selectors
+  - tag name
+  - id #
+  - class .
 
-let promptName = prompt("Enter your name: ", "Example") // "Example" is written here as a default value in the prompt
-console.log(promptName)
-document.write("Hello "+promptName)
+*/
 
-console.log(document.title)
+//! tag name
+
+const body = document.querySelector("body") // selects the first element matched
+// console.log(body) // returns HTMLBodyElement
+// body.innerHTML = "<h1>Hacked</h1>"
+// console.log(body.innerHTML) // returns String
+const code = document.querySelector("code")
+// code.innerHTML = "alert('Hello World')"
+
+//! id 
+
+const username = document.querySelector("#username")
+// username.innerText = "SDas"
+
+//! class
+
+const block = document.querySelector(".block")
+// console.log(block)
+
+const div = document.querySelector("div")
+// console.log(div.parentElement.children)
+
+const p = document.querySelectorAll("p")
+p.forEach((ele) => {
+    console.log(ele.innerHTML)
+})
