b5aa111615f45c7816d8c86413bcd4b39cf4cbe6
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-17 23:38:10 +0530

    document.createElement()

--- 11_DOM/script.js
+++ 11_DOM/script.js
@@ -9,30 +9,30 @@
 
 //! tag name
 
-const body = document.querySelector("body"); // selects the first element matched
+// const body = document.querySelector("body"); // selects the first element matched
 // console.log(body) // returns HTMLBodyElement
 // body.innerHTML = "<h1>Hacked</h1>"
 // console.log(body.innerHTML) // returns String
-const code = document.querySelector("code");
+// const code = document.querySelector("code");
 // code.innerHTML = "alert('Hello World')"
 
 //! id
 
-const username = document.querySelector("#username");
+// const username = document.querySelector("#username");
 // username.innerText = "SDas"
 
 //! class
 
-const block = document.querySelector(".block");
+// const block = document.querySelector(".block");
 // console.log(block)
 
-const div = document.querySelector("div");
+// const div = document.querySelector("div");
 // console.log(div.parentElement.children)
 
-const p = document.querySelectorAll("p");
-p.forEach((ele) => {
-  // console.log(ele.innerHTML)
-});
+// const p = document.querySelectorAll("p");
+// p.forEach((ele) => {
+//   // console.log(ele.innerHTML)
+// });
 
 // const ele = document.getElementById("username")
 // console.log(ele.innerText = "SDas")
@@ -47,7 +47,7 @@ p.forEach((ele) => {
 
 // ele.remove() // remove the whole element
 
-const ele = document.getElementsByClassName("block");
+// const ele = document.getElementsByClassName("block");
 
 // console.log(ele)
 
@@ -57,11 +57,11 @@ const ele = document.getElementsByClassName("block");
 //     }
 // }
 
-function handleButtonClick() {
-  alert("Btn clicked");
-}
+// function handleButtonClick() {
+//   alert("Btn clicked");
+// }
 
-const btn = document.getElementById("clickButton");
+// const btn = document.getElementById("clickButton");
 // btn.onclick = function () {
 //     alert("Btn clicked from function")
 // }
@@ -76,16 +76,26 @@ const btn = document.getElementById("clickButton");
 //     alert("Btn clicked from arrow function")
 // })
 
-const userName = document.getElementById("username");
+// const userName = document.getElementById("username");
 
-btn.addEventListener("click", () => {
-  if (userName.style.color === "red") userName.style.color = "blue";
-  else userName.style.color = "red";
+// btn.addEventListener("click", () => {
+//   if (userName.style.color === "red") userName.style.color = "blue";
+//   else userName.style.color = "red";
+// });
+
+// const allChildren = document.querySelector("body").children;
+// for (let i = 0; i < allChildren.length; i++) {
+//   allChildren.item(i).addEventListener("click", () => {
+//     allChildren.item(i).remove();
+//   });
+// }
+
+const container = document.getElementById("container");
+
+const clickButton = document.getElementById("clickButton");
+clickButton.addEventListener("click", () => {
+  // container.innerHTML = "<h1>Container Appeared</h1>"; // this only creates the quoted line one time
+  const el = document.createElement("h1"); // but by this method we can create same element again and again each time the button is clicked
+  el.innerText = "Soumyodeep Das";
+  container.appendChild(el);
 });
-
-const allChildren = document.querySelector("body").children;
-for (let i = 0; i < allChildren.length; i++) {
-  allChildren.item(i).addEventListener("click", () => {
-    allChildren.item(i).remove();
-  });
-}
