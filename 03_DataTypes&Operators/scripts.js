ae4dd4017d7ba7e3ce278242aa4a977e3b727563
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-17 17:42:49 +0530

    03 DataTypes and Basic Arithmatic Operators Completed

---
+++ 03_DataTypes&Operators/scripts.js
@@ -0,0 +1,57 @@
+// DataTypes in JavaScript
+/*
+
+1. Numbers (any numbers like int, float, long etc)
+let num1 = 2
+let num2 = 3
+console.log(num1+num2)
+
+
+2. Strings (any Alphanumeric strings)
+let firstName = 'Soumyodeep'
+let lastName = "Das"
+console.log(firstName+' '+lastName)
+
+string '1' + '1' -> '11' string
+string + number = string
+string * number = number
+
+3. Booleans (true | false) || ( 1 | 0)
+let isLoggedIn = false
+console.log(isLoggedIn)
+
+console.log(false + 10) --> 10
+        false -> 0      + 10 = 10
+console.log(true + 10) --> 10
+        true -> 1      + 10 = 11
+
+In JS, true = 1 and false = 0
+
+4. NULL (any unassigned or uninitialised variable)()null is an object
+
+5. Undefined (similar to NULL, but Undefined variables don't take any memory space)
+
+
+6. Objects (containes key value pairs)
+const person = {
+firstName : 'Soumyodeep',
+lastName : 'Das',
+age : 22,
+isLoggedIn: false,
+}
+
+*/
+
+// Opearators (Basic Arithmatic Operator)
+/*
+
++
+-
+*
+/
+
+TypeOf operator
+console.log(typeof '123')
+*/
+
+// NaN is Not a Number
